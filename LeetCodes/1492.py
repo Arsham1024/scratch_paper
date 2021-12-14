3ba1ee8c4db79a1490c9b666006505015a0c4958
Arsham Mehrani <Arsham512@gmail.com>
2021-12-14 13:40:21 -0800

    optimized speed of 1492 to 90%

--- LeetCodes/1492.py
+++ LeetCodes/1492.py
@@ -11,7 +11,31 @@ def version1(n , k):
     print(factors)
     return factors[k-1] if len(factors) >= k else -1
 
+# Clever version but not intuitive!
+# Speed: faster than 5%
+# Space: Less than 13.7%
+def version2(n , k):
+    for i in range(1, n+1):
+        if n % i == 0:
+            k -= 1
+            if k == 0:
+                return i
+    return -1
+
+# just including the first if statement makes this algorithm faster than 90%!!!!!!!!!
+# Speed: faster than 90.85%
+# Space: less than 13%
+def version3(n,k):
+    count = 0
+    if k == 1: return 1
+    for i in range(1, n+1):
+        if n % i == 0:
+            count += 1
+            if count == k:
+                return i
+    return -1
+
 if __name__ == "__main__":
     n = 7
     k = 2
-    print(version1(n , k))
+    print(version3(n , k))
