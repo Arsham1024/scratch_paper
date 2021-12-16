e5e6d45c20f86240d51403ca8e0f39957a317b71
Arsham Mehrani <Arsham512@gmail.com>
2021-12-15 16:37:49 -0800

    leetcode 1 done, brute force

---
+++ LeetCodes/1.py
@@ -0,0 +1,16 @@
+nums = [5,4,-1,7,8]
+target = 12
+
+def version1():
+    for i in range(len(nums) - 1):
+        sum = 0
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+
+def version2():
+    
+
+
+if __name__ == "__main__":
+    print(version1())
