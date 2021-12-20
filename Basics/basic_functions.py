2648028a7d94ca6acf484356b1de791015e17fa8
Arsham Mehrani <Arsham512@gmail.com>
2021-12-20 13:23:45 -0800

    more basic functions

--- Basics/basic_functions.py
+++ Basics/basic_functions.py
@@ -19,8 +19,38 @@ def find_dups():
     return list(dups)
 
 # check if anagram
-def is_anogram(s1 , s2):
+def is_anagram(s1 , s2):
     return set(s1) == set(s2)
+
+def is_anagram2(s1,s2):
+    if len(s1) != len(s2):
+        return False
+
+    s1 = list(s1)
+    s2 = list(s2)
+
+    for i, letter in enumerate(s1):
+        if letter in s2:
+            s2.remove(letter)
+            continue
+        else:
+            return False
+    return len(s2) == 0
+
+def reverse_string_r(string):
+    if len(string) <= 1:
+        return string
+
+    return reverse_string_r(string[1:]) + string[0]
+
+def reverse_string_s(string):
+    stack = list(string)
+    st = ""
+
+    for i in range(len(stack)):
+        st += stack.pop()
+
+    return st
 
 
 if __name__ == "__main__":
@@ -40,6 +70,17 @@ if __name__ == "__main__":
     # is anagram
     s1 = "elvis"
     s2 = "lives"
+    print(f"{s1} is an anagram of {s2}" if is_anagram(s1,s2) else f"{s1} is NOT an anagram of {s2}")
 
-    print(f"{s1} is an anagram of {s2}" if is_anogram(s1,s2) else f"{s1} is NOT an anagram of {s2}")
+    # detailed implementation
+    s1 = "elvis"
+    s2 = "lives"
+    print(f"{s1} is an anagram of {s2}" if is_anagram2(s1,s2) else f"{s1} is NOT an anagram of {s2}")
+
+    # reverse string using recursion
+    st = "hello"
+    print(reverse_string_r(st))
+
+    # reverse string using stack
+    print(reverse_string_s(st))
 
