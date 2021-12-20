87a58e2461c7037fa1ba7db381ba2a0eed14730a
Arsham Mehrani <Arsham512@gmail.com>
2021-12-19 20:44:59 -0800

    better version of 350 a bit faster

--- LeetCodes/350.py
+++ LeetCodes/350.py
@@ -2,7 +2,7 @@
 nums1 = [4,9,5]
 nums2 = [9,9]
 
-def intersection():
+def version1():
     intersect = []
 
     for i in nums1:
@@ -12,6 +12,27 @@ def intersection():
 
     return intersect
 
+def version2():
+    intersect = []
+
+    nums1.sort()
+    nums2.sort()
+
+    i, j = 0,0
+
+    while i < len(nums1) and j < len(nums2):
+        if nums1[i] == nums2[j]:
+            intersect.append(nums1[i])
+            i+=1
+            j+=1
+
+        elif nums1[i] < nums2[j]:
+            i+=1
+        else:
+            j+=1
+
+    return intersect
+
 
 if __name__ == "__main__":
-    print(intersection())
+    print(version2())
