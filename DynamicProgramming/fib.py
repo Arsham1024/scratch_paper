b22b1df84ea4787f029419bfcc16c571454e1f6a
Arsham Mehrani <Arsham512@gmail.com>
2021-12-21 11:57:03 -0800

    optimal space complexity for fib

--- DynamicProgramming/fib.py
+++ DynamicProgramming/fib.py
@@ -10,16 +10,31 @@ def solve_fib(n):
 
     return fibs[n]
 
+# Uses only 3 variables for optimal space complexity 
+def solve_fib2(n):
+    if n == 1: return 1
+    elif n == 2: return 1
+
+#     just remember last two values
+    n_1 = 1
+    n_2 = 0
+    answer = 0
+
+    for i in range(n-1):
+        n_2 = n_1
+        n_1 = answer
+        answer = n_1 + n_2
+        print(n_2 , n_1)
+
+    return answer
 if __name__ == '__main__':
 
     testcases = [
         3,
         6,
         10,
-        20,
-        40,
-        50
     ]
 
     for n in testcases:
-        print(f"The {n}th fib is {solve_fib(n)}")
+        # print(f"The {n}th fib is {solve_fib(n)}")
+        print(f"The {n}th fib is {solve_fib2(n)}")
