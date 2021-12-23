d67aec76b771b36a12ce607c6bef533fdc8d7054
Arsham Mehrani <Arsham512@gmail.com>
2021-12-22 20:22:31 -0800

    Clean up

--- LeetCodes/322_Coinchange.py
+++ LeetCodes/322_Coinchange.py
@@ -1,5 +1,6 @@
 
-def coin_change_brute(n):
+# Using dynamic programming
+def coin_change_DP(n):
     buffer = [n+1]*(n+1)
     buffer[0] = 0
 
@@ -8,10 +9,10 @@ def coin_change_brute(n):
             if amount - coin >= 0:
                 buffer[amount] = min(buffer[amount], 1+buffer[amount-coin])
 
-    return buffer[amount] if buffer[amount] != amount+1 else -1
+    return buffer[n] if buffer[n] != n+1 else -1
 
 
 if __name__ == '__main__':
     n = 11
     coins = [1, 2, 5]
-    print(f"fewest number of coins needed to make {n} is {coin_change_brute(n)}")
+    print(f"fewest number of coins needed to make {n} is {coin_change_DP(n)}")
