724f19b7ac8dc472780e9e5176a33e4a0d1a28c7
Arsham Mehrani <Arsham512@gmail.com>
2021-12-28 11:00:01 -0800

    main added

--- LeetCodes/27_Remove_Element.py
+++ LeetCodes/27_Remove_Element.py
@@ -1,9 +1,15 @@
-count = 0
-for i, item in enumerate(nums):
-    if item == val:
-        count += 1
-        nums[i] = -1
 
-print(nums.sort(reverse=True))
-return len(nums) - count
+def removeelements(nums, val):
+    count = 0
+    for i, item in enumerate(nums):
+        if item == val:
+            count += 1
+            nums[i] = -1
 
+    print(nums.sort(reverse=True))
+    return len(nums) - count
+
+if __name__ == '__main__':
+    list = [0,1,2,2,3,0,4,2]
+    val = 2
+    print(removeelements(list, val))
