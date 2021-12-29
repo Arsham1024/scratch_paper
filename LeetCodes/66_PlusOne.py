a0aa9d3f545c61f6f3141f8af086fa334e7c59b6
Arsham Mehrani <Arsham512@gmail.com>
2021-12-29 14:29:34 -0800

    Comments and more test cases for 66

--- LeetCodes/66_PlusOne.py
+++ LeetCodes/66_PlusOne.py
@@ -1,20 +1,28 @@
 def plus1(digits):
-    first_i = -1
-    length = len(digits)
 
+    # The first case where the number on the least sig digit is not 9
+    # 1-8 numbers can be +1 with no implications on the rest of the digits
     if digits[-1] != 9:
         digits[-1] += 1
         return digits
 
+    # traverse through the list in reverse order so we can logically assess the numbers
     for i in reversed(range(len(digits))):
+        # if the current number is 9
+        # check if this number is the last number! in that case make the current number 1 and append one 0 to the end
+        # This way we make the number bigger.
+
         if digits[i] == 9:
-            digits[i] = 0
             if i == 0:
                 digits[i] = 1
                 digits.append(0)
                 return digits
+            # if this index is not the end then just make the number 0 and continue to the next digit
+            # until we get to a digit that is less than 9 or end of the digits
+            digits[i] = 0
             continue
 
+        # if next digit is not 9 then just add one to it like before
         elif digits[i] != 9:
             digits[i] += 1
             return digits
